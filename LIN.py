e7c18c2f4eccfb2a6818c72c1a0ec95f5a8f598e
Aditya Sinha <adityasinha379@gmail.com>
2019-04-30 16:47:25 -0400

    LIN model

---
+++ LIN.py
@@ -0,0 +1,120 @@
+from collections import OrderedDict
+
+import numpy as np
+
+import pycuda.gpuarray as garray
+from pycuda.tools import dtype_to_ctype
+import pycuda.driver as cuda
+from pycuda.compiler import SourceModule
+
+from BaseAxonHillockModel import BaseAxonHillockModel
+
+class LeakyIAF(BaseAxonHillockModel):
+    updates = [ 'V']
+    accesses = ['I']
+    params = ['resting_potential','tau']
+    internals = OrderedDict([('internalV',0.0)])
+
+    def __init__(self, params_dict, access_buffers, dt,
+                 debug=False, LPU_id=None, cuda_verbose=False):
+        if cuda_verbose:
+            self.compile_options = ['--ptxas-options=-v']
+        else:
+            self.compile_options = []
+
+        self.num_comps = params_dict['resting_potential'].size
+        self.params_dict = params_dict
+        self.access_buffers = access_buffers
+        self.dt = np.double(dt)
+        self.steps = 1
+        self.debug = debug
+        self.LPU_id = LPU_id
+        self.dtype = params_dict['resting_potential'].dtype
+        self.ddt = self.dt/self.steps
+
+        self.internal_states = {
+            c: garray.zeros(self.num_comps, dtype = self.dtype)+self.internals[c] \
+            for c in self.internals}
+
+        self.inputs = {
+            k: garray.empty(self.num_comps, dtype = self.access_buffers[k].dtype)\
+            for k in self.accesses}
+
+        dtypes = {'dt': self.dtype}
+        dtypes.update({k: self.inputs[k].dtype for k in self.accesses})
+        dtypes.update({k: self.params_dict[k].dtype for k in self.params})
+        dtypes.update({k: self.internal_states[k].dtype for k in self.internals})
+        dtypes.update({k: self.dtype if not k == 'spike_state' else np.int32 for k in self.updates})
+        self.update_func = self.get_update_func(dtypes)
+
+    def pre_run(self, update_pointers):
+        if self.params_dict.has_key('initV'):
+            cuda.memcpy_dtod(int(update_pointers['V']),
+                             self.params_dict['initV'].gpudata,
+                             self.params_dict['initV'].nbytes)
+            cuda.memcpy_dtod(self.internal_states['internalV'].gpudata,
+                             self.params_dict['initV'].gpudata,
+                             self.params_dict['initV'].nbytes)
+
+        else:
+            cuda.memcpy_dtod(int(update_pointers['V']),
+                             self.params_dict['resting_potential'].gpudata,
+                             self.params_dict['resting_potential'].nbytes)
+            cuda.memcpy_dtod(self.internal_states['internalV'].gpudata,
+                             self.params_dict['resting_potential'].gpudata,
+                             self.params_dict['resting_potential'].nbytes)
+
+    def run_step(self, update_pointers, st=None):
+        for k in self.inputs:
+            self.sum_in_variable(k, self.inputs[k], st=st)
+
+        self.update_func.prepared_async_call(
+            self.update_func.grid, self.update_func.block, st,
+            self.num_comps, self.ddt*1000, self.steps,
+            *[self.inputs[k].gpudata for k in self.accesses]+\
+            [self.params_dict[k].gpudata for k in self.params]+\
+            [self.internal_states[k].gpudata for k in self.internals]+\
+            [update_pointers[k] for k in self.updates])
+
+    def get_update_template(self):
+        template = """
+__global__ void update(int num_comps, %(dt)s dt, int nsteps,
+               %(I)s* g_I,
+               %(resting_potential)s* g_resting_potential,
+               %(tau)s* g_tau,
+               %(internalV)s* g_internalV, %(V)s* g_V)
+{
+    int tid = threadIdx.x + blockIdx.x * blockDim.x;
+    int total_threads = gridDim.x * blockDim.x;
+    %(V)s V;
+    %(I)s I;
+    %(resting_potential)s resting_potential;
+    %(tau)s tau;
+    %(dt)s bh;
+    for(int i = tid; i < num_comps; i += total_threads)
+    {
+        V = g_internalV[i];
+        I = g_I[i];
+        resting_potential = g_resting_potential[i];
+        tau = g_tau[i];
+
+        V = (-V+I)/tau*dt;
+        
+        g_V[i] = V;
+        g_internalV[i] = V;
+    }
+}
+        """
+        return template
+
+    def get_update_func(self, dtypes):
+        type_dict = {k: dtype_to_ctype(dtypes[k]) for k in dtypes}
+        type_dict.update({'fletter': 'f' if type_dict['resting_potential'] == 'float' else ''})
+        mod = SourceModule(self.get_update_template() % type_dict,
+                           options=self.compile_options)
+        func = mod.get_function("update")
+        func.prepare('i'+np.dtype(dtypes['dt']).char+'i'+'P'*(len(type_dict)-2))
+        func.block = (256,1,1)
+        func.grid = (min(6 * cuda.Context.get_device().MULTIPROCESSOR_COUNT,
+                         (self.num_comps-1) / 256 + 1), 1)
+        return func
